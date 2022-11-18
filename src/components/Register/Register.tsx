91cdfd1198eeb570f1e9e02a59d2fabd5dfea8bc
Mateusz <nowe7246@gmail.com>
2022-11-18 11:18:26 +0100

    Added react-hook-form to register form.

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,14 +1,61 @@
+import {useForm} from "react-hook-form";
+import {emailValidate, passwordValidate} from "../../constants/validate";
+import {emailErrorDesc, passwordErrorDesc} from "../../constants/description";
+
+type RegisterFormType = {
+    email: string
+    password: string
+}
 export const Register = () => {
+    const {
+        handleSubmit,
+        register,
+        formState: {
+            errors: {email, password},
+        },
+    } = useForm<RegisterFormType>({
+        defaultValues: {
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit = ({email, password}: RegisterFormType) => {
+        console.log(email, password, 'sign up')
+    }
     return (
         <div className='form_container'>
             <h2>Sign Up to Book Seat</h2>
-            <label>
-                <input  type="email" placeholder='Email'/>
-            </label>
-            <label>
-                <input type="password" placeholder='Password'/>
-            </label>
-            <button>Sign up</button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    <input type="email"
+                           {...register('email', {
+                               required: "Email is required",
+                               pattern: {
+                                   value: emailValidate,
+                                   message: emailErrorDesc,
+                               },
+                           })}
+                           placeholder='Email'
+                    />
+                    {email ? <div>{email.message}</div> : null}
+                </label>
+                <label>
+                    <input
+                        type="password"
+                        {...register('password', {
+                            required: `Password is required`,
+                            pattern: {
+                                value: passwordValidate,
+                                message: passwordErrorDesc
+                            },
+                        })}
+                        placeholder='Password'
+                    />
+                    {password ? <div>{password.message}</div> : null}
+                </label>
+                <button>Sign up</button>
+            </form>
         </div>
     )
 }
