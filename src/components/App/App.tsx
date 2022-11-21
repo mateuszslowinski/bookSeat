75e003e045cbbf6b358cad5efde98c1b147e6973
Mateusz <nowe7246@gmail.com>
2022-11-21 14:45:49 +0100

    Used sign form in login and register components.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {HomePage} from "../../pages/Home/Home.page";
-import {Register} from "../Register/Register";
+import {Register} from "../SignForm/Register/Register";
+import {Login} from '../SignForm/Login/Login';
 import "../../sass/main.scss";
-import {Login} from '../Login/Login';
 
 export const App = () => (
     <Routes>
---
+++ src/components/SignForm/Login/Login.tsx
@@ -0,0 +1,22 @@
+import {useDispatch} from "react-redux";
+import {SignForm} from "../SignForm";
+import {userLogin} from "../../../features/user/userActions";
+import {UserLoginType} from 'types';
+
+
+export const Login = () => {
+    const dispatch = useDispatch();
+    const onSubmit = async (data: UserLoginType) => {
+        const {email, password} = data
+        if (email && password) {
+            dispatch(userLogin({email, password}))
+        }
+    }
+    return (
+        <SignForm
+            onSubmit={onSubmit}
+            btnText='Sign In'
+            formSubtitle='Sign In to Book Seat'
+        />
+    )
+}
