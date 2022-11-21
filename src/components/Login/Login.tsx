4c4bc15f2a8225dcb019f8b19a55173acfe55efc
Mateusz <nowe7246@gmail.com>
2022-11-21 14:27:56 +0100

    Used text input comopnents in login form.

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,8 +2,9 @@ import {emailValidate, passwordValidate} from "../../constants/validate";
 import {emailErrorDesc, passwordErrorDesc} from "../../constants/description";
 import {useForm} from "react-hook-form";
 import {useDispatch} from "react-redux";
-import {UserLoginType} from 'types';
+import {TextInputField} from "../common/TextInputField/TextInputField";
 import {userLogin} from "../../features/user/userActions";
+import {UserLoginType} from 'types';
 
 
 export const Login = () => {
@@ -27,40 +28,33 @@ export const Login = () => {
             dispatch(userLogin({email, password}))
         }
     }
-
-
     return (
         <div className='form_container'>
             <h2>Sign In to Book Seat</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <label>
-                    <input type="email"
-                           {...register('email', {
-                               required: "Email is required",
-                               pattern: {
-                                   value: emailValidate,
-                                   message: emailErrorDesc,
-                               },
-                           })}
-                           placeholder='Email'
-                    />
-                    {email ? <div>{email.message}</div> : null}
-                </label>
-                <label>
-                    <input
-                        type="password"
-                        {...register('password', {
-                            required: `Password is required`,
-                            pattern: {
-                                value: passwordValidate,
-                                message: passwordErrorDesc
-                            },
-                        })}
-                        placeholder='Password'
-                    />
-                    {password ? <div>{password.message}</div> : null}
-                </label>
-                <button>Sign In</button>
+                <TextInputField
+                    type='email'
+                    validation={register('email', {
+                        required: "Email is required",
+                        pattern: {
+                            value: emailValidate,
+                            message: emailErrorDesc,
+                        },
+                    })}
+                    placeholder='Email' error={email}
+                />
+                <TextInputField
+                    type='password'
+                    validation={register('password', {
+                        required: `Password is required`,
+                        pattern: {
+                            value: passwordValidate,
+                            message: passwordErrorDesc
+                        },
+                    })} error={password}
+                    placeholder="Password"
+                />
+                <button> Sign In</button>
             </form>
         </div>
     )
