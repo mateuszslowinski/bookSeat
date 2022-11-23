3b6245fb5279864cc5636bb3238f807645e2ea50
Mateusz <nowe7246@gmail.com>
2022-11-23 13:10:01 +0100

    Added map into home page.

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Header} from "../../components/Header/Header";
+import {Map} from "../../components/Map/Map";
 
 export const HomePage = () => {
     return (
         <div className='page_container'>
             <Header/>
-            <p>hello world</p>
+            <Map />
         </div>
     )
 }
