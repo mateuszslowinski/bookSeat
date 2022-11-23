9ed1c2c4af93191f0a02680a17d0371d4a41839c
Mateusz <nowe7246@gmail.com>
2022-11-23 13:13:04 +0100

    Changed the start coordinates to Krakow and removed the disabled scroll on the map.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -4,7 +4,7 @@ import 'leaflet/dist/leaflet.css';
 export const Map = () => {
     return (
         <div className="map">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={[50.049683, 19.944544]} zoom={18}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
